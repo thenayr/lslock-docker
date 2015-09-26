6c9f106878f4e1b77e530c2d96fe9aad59a7bc88
thenayr <iamnayr@gmail.com>
2015-09-26 13:08:45 -0700

    Mapping inodes, counting total locks

--- lslock-test/go-filelock-test.go
+++ lslock-test/go-filelock-test.go
@@ -2,7 +2,7 @@ package main
 
 import (
   "os"
-	"bytes"
+	//"bytes"
 	"bufio"
   "os/exec"
   "strings"
@@ -13,15 +13,19 @@ import (
 
 const tmpLockDir string = "/tmp/lslock-test"
 var lockPath string
+var mLocks map[string]string
 
 func main() {
-
-	ReadLocks()	
+	mLocks = make(map[string]string)
 	ReadDirs()	
+	ReadLocks()	
+	fmt.Println("Printing mLocks:")
+	fmt.Println(mLocks)
 }
 
 
 // Get a listing of files and directories
+// Map filenames to iNodes (as strings)
 func ReadDirs() {
   files, _ := ioutil.ReadDir(tmpLockDir)
   for _, f := range files {
@@ -33,21 +37,26 @@ func ReadDirs() {
         if err != nil {
 					fmt.Println(err)
         }
-        //fmt.Printf("Locked files are \n %s\n", out)
-        fmt.Printf("File is " + f.Name() + " - Inode is: %q\n", bytes.Split(out, []byte{' '})[2])
+				cOut := string(out)
+				iN := strings.Split(cOut, " ")[0]
+        mLocks[f.Name()] = iN
+				fmt.Printf("iN is equal to : %q\n", iN)
       }
   }
 }
 
 func ReadLocks() {
   // Open the file and scan it.
+	// '/proc/locks' is mounted to /go/locks
   f, _ := os.Open("/go/locks")
   scanner := bufio.NewScanner(f)
+	fileCount := 0
 
   for scanner.Scan() {
     line := scanner.Text()
     // Split the line on commas.
     parts := strings.Split(line, "\n")
+		fileCount++
     // Loop over the parts from the string.
     for i := range parts {
 			parts[i] = strings.Split(strings.Split(parts[i], ":")[3], " ")[0]
@@ -56,4 +65,5 @@ func ReadLocks() {
     // Write a newline.
     fmt.Println()
   }
+	fmt.Printf("Total file count: %d", fileCount)
 }
